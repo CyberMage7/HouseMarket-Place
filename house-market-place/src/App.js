3604a7af2ae778a096af0e0ee7a2a3b39d184d69
CyberMage7 <technician9012@gmail.com>
2024-07-01 23:54:31 +0530

    Added Pages and routers

--- house-market-place/src/App.js
+++ house-market-place/src/App.js
@@ -1,9 +1,26 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
+import Explore from './pages/Explore';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Explore />} />
+        <Route path='/offers' element={<Offers />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+      </Routes>
     <h1>My app</h1>
+    </Router>
     </>
   );
 }
