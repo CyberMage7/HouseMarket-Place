ee8864a243651834fef5808f4d607fff863dc4a8
CyberMage7 <technician9012@gmail.com>
2024-07-02 12:38:57 +0530

    Added react-toastify package

--- house-market-place/src/App.js
+++ house-market-place/src/App.js
@@ -1,27 +1,30 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-
-import Explore from './pages/Explore';
-import ForgotPassword from './pages/ForgotPassword';
-import Offers from './pages/Offers';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Profile from './pages/Profile';
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Explore from "./pages/Explore";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-    <Router>
-    <Navbar />
-      <Routes>
-        <Route path='/' element={<Explore />} />
-        <Route path='/offers' element={<Offers />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/forgot-password' element={<ForgotPassword />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+        <Navbar />
+      </Router>
+
+      <ToastContainer />
     </>
   );
 }
